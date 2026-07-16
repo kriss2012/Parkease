10ed303792dfd0e4ba17e4cf0c72f62e1536b760
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-07-16 10:36:02 +0530

    Create generateToken.js

---
+++ server/src/utils/generateToken.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+const generateAccessToken = (id, role) =>
+  jwt.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE || '15m',
+  });
+
+const generateRefreshToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRE || '7d',
+  });
+
+module.exports = { generateAccessToken, generateRefreshToken };
